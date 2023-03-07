9346de407ea6bcb88ae2051b0995af698f9c4e34
AIE\s210179 <harrybosch03@gmail.com>
2023-03-07 15:39:44 +1100

    Fixed Missing Dependencies

---
+++ Code/Scripts/Editor/Quick Actions/Extensions.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+
+namespace BoschingMachine.Editor.QuickActions
+{
+    public static class Extensions
+    {
+        public static T GetOrAddComponent<T>(this GameObject gameObject) where T : Component
+        {
+            if (gameObject.TryGetComponent(out T component))
+            {
+                return component;
+            }
+            else return gameObject.AddComponent<T>();
+        }
+
+        public static T DeepFindCallback<T>(this Transform transform, string name, System.Func<Transform, T> callback)
+        {
+            var result = transform.DeepFind(name);
+            if (result) return callback(result);
+            else return default;
+        }
+
+        public static void DeepFindCallback(this Transform transform, string name, System.Action<Transform> callback)
+        {
+            var result = transform.DeepFind(name);
+            if (result) callback(result);
+        }
+
+        public static Transform DeepFind(this Transform transform, string name)
+        {
+            var result = transform.Find(name);
+            if (result) return result;
+
+            foreach (Transform child in transform)
+            {
+                result = child.DeepFind(name);
+                if (result) return result;
+            }
+
+            return null;
+        }
+
+        public static string Replace(this string text, System.Func<char, bool> condition, System.Func<char, string> replace)
+        {
+            return text.Replace((i, s) => condition(s[i]), replace);
+        }
+        public static string Replace(this string text, System.Func<int, string, bool> condition, System.Func<char, string> replace)
+        {
+            var head = 0;
+
+            while (head < text.Length)
+            {
+                if (condition(head, text))
+                {
+                    var pre = text.Substring(0, head);
+                    var post = head < text.Length - 1 ? text.Substring(head + 1, text.Length - (head + 1)) : string.Empty;
+                    var mid = replace(text[head]);
+                    text = pre + mid + post;
+                    head += mid.Length;
+                }
+                else head++;
+            }
+
+            return text;
+        }
+        public static string Replace(this string text, char to, params char[] list)
+        {
+            var arr = text.ToCharArray();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = to;
+            }
+            return new string(arr);
+        }
+
+        public static bool IsCapital(this char c)
+        {
+            return c - 65 < 26;
+        }
+    }
+}
---
+++ Code/Scripts/Editor/Quick Actions/Util.cs
@@ -0,0 +1,29 @@
+using System.IO;
+
+namespace BoschingMachine.Editor.QuickActions
+{
+    public class Util
+    {
+        public static string PrettifyName(string text)
+        {
+            text = text.Replace(c => c.IsCapital(), c => $" {c}").Replace(' ', '_', '.', '-').Trim();
+            text = text[0].ToString().ToUpper() + text.Substring(1, text.Length - 1);
+            return text;
+        }
+
+        public static bool IsChildDirectoryOf(string child, string parent)
+        {
+            child = Path.GetFullPath(child);
+            parent = Path.GetFullPath(parent);
+
+            if (parent.Length > child.Length) return false;
+
+            for (int i = 0; i < parent.Length; i++)
+            {
+                if (child[i] != parent[i]) return false;
+            }
+
+            return true;
+        }
+    }
+}
